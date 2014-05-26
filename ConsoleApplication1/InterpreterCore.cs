5b0fba50f0050f61d88e915d59592cfc469c8cac
Vasilii Pavlov <vasiliipavlov@hotmail.com>
2014-05-26 04:17:47 +0400

    Добавление файлов в проект

---
+++ ConsoleApplication1/InterpreterCore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    public class InterpreterCore
+    {
+        public void Initialization()
+        {
+            Console.WriteLine("Enter command:");
+            CommandReader();
+        }
+
+        public void CommandReader()
+        {
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InterpreterCore interp = new InterpreterCore();
+            interp.Initialization();
+        }
+    }
+}
